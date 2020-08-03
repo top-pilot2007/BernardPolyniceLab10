cebfefd0783e42be0676d7f93a60ba997a95f3a1
Bernard Polynice <bepoly.nice@gmail.com>
2020-08-03 18:12:15 -0400

    First Commit

---
+++ BernardLab10/Circle.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace BernardLab10
+{
+
+    public class Circle
+    {
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public double CalculateCircumference()
+        {
+            // 2 *pi*radius
+            //Radius = radius;
+            var circumference = (2 * Math.PI) * Radius;
+            Circumference = circumference;
+            return Circumference;
+        }
+        public string CalculateFormattedCircumference()
+        {
+            CalculateCircumference();
+            string newFormatCircumference = FormatNumber(Circumference);
+            return newFormatCircumference;
+        }
+        public double CalculateArea()
+        {
+            //    //formula for area of a circle 2 pie r squared
+            //    // c squared / 4pi
+            Area = ((Circumference * Circumference) / (4 * Math.PI));
+            return Area;
+        }
+        public string CalculateFormattedArea()
+        {
+            CalculateArea();
+            string newFormatArea = FormatNumber(Area);
+            return newFormatArea;
+        }
+        private string FormatNumber(double x)
+        {
+            double twoDecimalPlace = Math.Round(x, 2);
+            var correctFormat = twoDecimalPlace.ToString();
+            CorrectFormat = correctFormat;
+            return CorrectFormat;
+        }
+
+        private double _radius;
+
+        public double Radius
+        {
+            get { return _radius; }
+            set { _radius = value; }
+        }
+
+        public double Circumference { get; set; }
+        public double Area { get; set; }
+        public string CorrectFormat { get; set; }
+
+        public static bool UserContinueSelection { get; set; } = true;
+        public static void UserContinue()
+        {
+            Console.Write("Continue? (y/n): ");
+            var userChoice = Console.ReadLine();
+
+            if (userChoice.Contains("Y", StringComparison.InvariantCultureIgnoreCase))
+            {
+                UserContinueSelection = true;
+            }
+            else
+            {
+                UserContinueSelection = false;
+            }
+        }
+
+
+    }
+}
---
+++ BernardLab10/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace BernardLab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Circle Tester");
+
+            int circleCount = 0;
+            while (Circle.UserContinueSelection == true)
+            {
+                circleCount++;
+                
+                                    Console.Write("Enter radius: ");
+
+                try
+                {
+                    var userRadius = Double.Parse(Console.ReadLine());
+                
+                    var circle = new Circle(userRadius);
+
+                    Console.WriteLine($"Circumference: {circle.CalculateFormattedCircumference()}");
+                    Console.WriteLine($"Area: {circle.CalculateFormattedArea()}");
+                
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                        //exception message
+                }
+
+                Circle.UserContinue();
+                
+
+             }
+            Console.WriteLine($"Goodbye. You created {circleCount} Circle object(s).");
+        }
+    }
+}
